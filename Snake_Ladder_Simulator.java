031e119b1c77fbf7dcbdfb1ccf68013cf077928f
parag <adhauparag64@gmail.com>
2020-08-21 02:53:24 +0530

    refracting the code

--- Snake_Ladder_Simulator.java
+++ Snake_Ladder_Simulator.java
@@ -8,15 +8,15 @@ import java.util.Random;
         private static int option = 0;
         private static int diceNumber = 0;
         private static int numberOfTimesDiceRoll = 0;
-
+    //writing method for getting dice number//
         public static int throwDice(){
-            numberOfTimesDiceRoll = numberOfTimesDiceRoll+1;
+            numberOfTimesDiceRoll = numberOfTimesDiceRoll+1;//for counting number of times dice roll//
             Random r=new Random();
             int random = r.nextInt(6)+1;
           //int r1 = 1 + (random % 6);
             return random;
          }
-
+    //writing method for getting option for no-play,ladder and snake//
         public static int optionCheck(){
             Random r1 = new Random();
             int random1 =r1.nextInt(3)+1;
@@ -29,22 +29,22 @@ import java.util.Random;
                 diceNumber=throwDice();
                 option=optionCheck();
 
-                if (option == 2){
+                if (option == 2){//for ladder//
 		  newPosition = newPosition+diceNumber;
-                  if(newPosition>100){
+                  if(newPosition>100){      //if current position becomes grater than 100//
                      newPosition = newPosition-diceNumber;
                    }
                  }
-		else if (option == 3){
+		else if (option == 3){//for snake//
 		  newPosition = newPosition-diceNumber;
-                    if(newPosition<0){
+                    if(newPosition<0){//if current position become less than 0//
                       newPosition=0;
                    }
                  }
-                else if (option == 1){
+                else if (option == 1){//for no-play//
                   newPosition=newPosition;
                  }
-
+            //peinting all the results//
                 System.out.println("Starting Position : "+ startPosn);
 		System.out.println("Ending Position : "+ endPosn);
                 System.out.println("New Position : "+ newPosition);
