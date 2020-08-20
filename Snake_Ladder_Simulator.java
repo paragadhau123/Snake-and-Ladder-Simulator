f7effb0cd4cf8cdd8495b4942475e5c7eebc2608
parag <adhauparag64@gmail.com>
2020-08-21 01:39:21 +0530

    adding functionality for checking option for no play,ladder and snake

--- Snake_Ladder_Simulator.java
+++ Snake_Ladder_Simulator.java
@@ -2,21 +2,42 @@ import java.util.Random;
 
  class Snake_Ladder_Simulator{
 
-	public static final int START_POSITION = 0;
-	public static final int END_POSITION = 100;
-
-        private static int newPosition = 0;
+	public static final int startPosn = 0;
+	public static final int endPosn = 100;
+        private static int newPosition = startPosn;
+        private static int option = 0;
+        private static int diceNumber = 0;
 
         public static int throwDice(){
             Random r=new Random();
             int random = r.nextInt(6)+1;
-//            int r1 = 1 + (random % 6);
+          //int r1 = 1 + (random % 6);
             return random;
          }
-	public static void main(String[] args) {
-		System.out.println("Starting Position : "+ START_POSITION);
-		System.out.println("Ending Position : "+ END_POSITION);
-                newPosition = newPosition+throwDice();
+
+        public static int optionCheck(){
+            Random r1 = new Random();
+            int random1 =r1.nextInt(3)+1;
+            return random1;
+          }
+
+ 	public static void main(String[] args) {
+
+                diceNumber=throwDice();
+                option=optionCheck();
+
+                if (option == 2){
+		  newPosition = newPosition+diceNumber;
+                 }
+		else if (option == 3){
+		  newPosition = newPosition-diceNumber;
+                 }
+                else if (option == 0){
+                  newPosition=newPosition;
+                 }
+
+                System.out.println("Starting Position : "+ startPosn);
+		System.out.println("Ending Position : "+ endPosn);
                 System.out.println("New Position : "+ newPosition);
 	}
    }
