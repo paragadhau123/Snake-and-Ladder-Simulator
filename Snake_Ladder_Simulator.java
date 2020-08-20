bee936849a3885e6050474a23f2d60bfe14dec6a
parag <adhauparag64@gmail.com>
2020-08-20 23:52:17 +0530

    initialising start and end position

---
+++ Snake_Ladder_Simulator.java
@@ -0,0 +1,10 @@
+class Snake_Ladder_Simulator{
+
+	public static final int START_POSITION = 0;
+	public static final int END_POSITION = 100;
+
+	public static void main(String[] args) {
+		System.out.println("Starting Position : "+ START_POSITION);
+		System.out.println("Ending Position : "+ END_POSITION);
+	}
+}
