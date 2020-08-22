a9ce4f1645f5ca7f419747a213d5eff8b10c3b81
parag <adhauparag64@gmail.com>
2020-08-22 22:35:04 +0530

    Refactored :Increase the output readability

--- Snake_Ladder_Simulator.java
+++ Snake_Ladder_Simulator.java
@@ -2,12 +2,12 @@ import java.util.Random;
 
  class Snake_Ladder_Simulator{
 
-	public static final int startPosn = 0;
-	public static final int endPosn = 100;
-        private static int newPosition = startPosn;
-        private static int option = 0;
-        private static int diceNumber = 0;
-        private static int numberOfTimesDiceRoll = 0;
+        static final int startPosn = 0;
+        static final int endPosn = 100;
+        static int newPosition = startPosn;
+        static int option = 0;
+        static int diceNumber = 0;
+        static int numberOfTimesDiceRoll = 0;
     //writing method for getting dice number//
         public static int throwDice(){
             numberOfTimesDiceRoll = numberOfTimesDiceRoll+1;//for counting number of times dice roll//
@@ -24,31 +24,42 @@ import java.util.Random;
           }
 
  	public static void main(String[] args) {
+             System.out.println("Starting Position : "+ startPosn);
+             System.out.println("Ending Position : "+ endPosn);
+
              while(newPosition<endPosn){
 
                 diceNumber=throwDice();
                 option=optionCheck();
 
+                System.out.println("Dice Number is :"+diceNumber);
+
                 if (option == 2){//for ladder//
+                  System.out.println("Option is for Ladder");
 		  newPosition = newPosition+diceNumber;
                   if(newPosition>100){      //if current position becomes grater than 100//
                      newPosition = newPosition-diceNumber;
+                     System.out.println("New position is going above 100 so it will be on same position");
                    }
                  }
 		else if (option == 3){//for snake//
+                  System.out.println("Option is for Snake");
 		  newPosition = newPosition-diceNumber;
                     if(newPosition<0){//if current position become less than 0//
                       newPosition=0;
+                      System.out.println("New position is going below 0 so it will again start from 0");
                    }
                  }
                 else if (option == 1){//for no-play//
+                  System.out.println("Option is No-Play");
                   newPosition=newPosition;
                  }
-            //peinting all the results//
-                System.out.println("Starting Position : "+ startPosn);
-		System.out.println("Ending Position : "+ endPosn);
-                System.out.println("New Position : "+ newPosition);
+
+                System.out.println("New Position is : "+ newPosition);
+                System.out.println("------------------------------------");
 	}
-                System.out.println("Number of times dice roll to win the game =>"+numberOfTimesDiceRoll);
+                System.out.println("*******The Game is ended as 100th position is reached********");
+                System.out.println("*******Number of times dice roll to win the game =>"+numberOfTimesDiceRoll+"**********");
      }
   }
+
